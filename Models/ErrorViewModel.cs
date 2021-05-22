8d4eb8bba232cad28beaff6e07f112286cd9a9c3
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-22 23:32:29 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppointmentSchedulerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
